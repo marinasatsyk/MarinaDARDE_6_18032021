c9fbd64fcc1a2229ea81a00a78c30637d0aff938
Marina DARDE <marinasatsyk@gmail.com>
2021-10-18 12:23:14 +0200

    attempt to manipulate with JSON file doesn't work

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-fetch('./data.json')
-    .then(data => {
-        console.log(data);
-    });
+// async function getData() {
+//     const dataFile = await fetch("./data.json");
+//     const data = await dataFile.json();
+//     return data;
+// }
+
+// // function getAJAX(data) {
+// //     console.log('request');
+// //     console.log('response');
+// //     console.log(data);
+// // }
+// console.log(getData());
