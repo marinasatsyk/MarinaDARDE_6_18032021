62e7025039f08fc187f4a3e4e86b67f2c21323be
Marina DARDE <marinasatsyk@gmail.com>
2021-11-26 16:45:02 +0100

    deleting drafts

--- js_pages/Factory/FactoryMedia.js
+++ js_pages/Factory/FactoryMedia.js
@@ -12,24 +12,3 @@ export class FactoryMedia {
         }
     }
 }
-
-
-
-
-/*
-function FactoryImage_(media) {
-    let folders = media.attributes[0].value.split("/");
-    let key = folders[folders.length - 1];
-    let obj = MAP_Media.get(key);
-    return `<img src=${media.attributes[0].value} alt="${obj.title}">
-	<div>${obj.title}</div>`;
-}
-
-function FactoryVideo_(media) {
-    let folders = media.attributes[0].value.split("/");
-    let key = folders[folders.length - 1];
-    let obj = MAP_Media.get(key);
-    return `<video src=${media.attributes[0].value} alt="${obj.title}" controls>
-	<div>${obj.title}</div>`;
-}
-*/
